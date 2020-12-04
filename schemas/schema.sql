9974e0be429f4bc55ce86d6422b8c24e8df156ed
xsw2-2wsx <jan1undrych@gmail.com>
2020-12-04 20:58:06 +0100

    Added database schema and api to version control

---
+++ schemas/schema.sql
@@ -0,0 +1,288 @@
+-- MySQL dump 10.17  Distrib 10.3.25-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: queue
+-- ------------------------------------------------------
+-- Server version	10.3.25-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `queue`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `queue` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `queue`;
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authorities` (
+  `authority_id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(30) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`authority_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cancel_date_changes`
+--
+
+DROP TABLE IF EXISTS `cancel_date_changes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cancel_date_changes` (
+  `change_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lesson_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`change_id`),
+  KEY `lessonid_index` (`lesson_id`),
+  CONSTRAINT `cdc_lessonid_to_lessons_id` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`lesson_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `date_changes`
+--
+
+DROP TABLE IF EXISTS `date_changes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `date_changes` (
+  `change_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lesson_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`change_id`),
+  KEY `lessonid_index` (`lesson_id`),
+  KEY `userid_index` (`user_id`),
+  CONSTRAINT `dc_lessonid_to_lessons_id` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `dc_userid_to_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `exchange_requests`
+--
+
+DROP TABLE IF EXISTS `exchange_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exchange_requests` (
+  `request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(11) NOT NULL,
+  `from_lesson_id` int(11) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `to_lesson_id` int(11) NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`request_id`),
+  KEY `from_userid_index` (`from_user_id`),
+  KEY `from_lessonid_index` (`from_lesson_id`),
+  KEY `to_userid_index` (`to_user_id`),
+  KEY `to_lessonid_index` (`to_lesson_id`),
+  CONSTRAINT `er_from_lessonid_to_lessons_id` FOREIGN KEY (`from_lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `er_from_userid_to_users_id` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `er_to_lessonid_to_lessons_id` FOREIGN KEY (`to_lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `er_to_userid_to_users_id` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `exchanges`
+--
+
+DROP TABLE IF EXISTS `exchanges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exchanges` (
+  `exchange_id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(11) DEFAULT NULL,
+  `from_lesson_id` int(11) DEFAULT NULL,
+  `from_date` date NOT NULL,
+  `to_user_id` int(11) DEFAULT NULL,
+  `to_lesson_id` int(11) DEFAULT NULL,
+  `to_date` date NOT NULL,
+  `accept_date` date NOT NULL,
+  `from_change_id` int(11) DEFAULT NULL,
+  `to_change_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`exchange_id`),
+  KEY `from_userid_index` (`from_user_id`),
+  KEY `from_lessonid_index` (`from_lesson_id`),
+  KEY `to_userid_index` (`to_user_id`),
+  KEY `to_lessonid_index` (`to_lesson_id`),
+  KEY `from_change_id_index` (`from_change_id`),
+  KEY `to_change_id_index` (`to_change_id`),
+  CONSTRAINT `ac_from_changeid_to_date_changes_id` FOREIGN KEY (`from_change_id`) REFERENCES `date_changes` (`change_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ac_from_lessonid_to_lessons_id` FOREIGN KEY (`from_lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `ac_from_userid_to_users_id` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `ac_to_changeid_to_date_changes_id` FOREIGN KEY (`to_change_id`) REFERENCES `date_changes` (`change_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ac_to_lessonid_to_lessons_id` FOREIGN KEY (`to_lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `ac_to_userid_to_users_id` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lessons`
+--
+
+DROP TABLE IF EXISTS `lessons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lessons` (
+  `lesson_id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_id` int(11) NOT NULL,
+  `lesson_index` int(11) NOT NULL,
+  `next_date` date NOT NULL,
+  `time` time NOT NULL,
+  `pointer` int(11) NOT NULL,
+  `recurrence_interval` int(11) NOT NULL,
+  PRIMARY KEY (`lesson_id`),
+  KEY `subjectid_index` (`subject_id`),
+  CONSTRAINT `l_subjectid_to_subjects_id` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `occurrence_log`
+--
+
+DROP TABLE IF EXISTS `occurrence_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `occurrence_log` (
+  `occurrence_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lesson_id` int(11) DEFAULT NULL,
+  `lesson_index` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`occurrence_id`),
+  KEY `lessonid_index` (`lesson_id`),
+  KEY `userid_index` (`user_id`),
+  CONSTRAINT `ol_lessonid_to_lessons_id` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `ol_userid_to_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `queue_users`
+--
+
+DROP TABLE IF EXISTS `queue_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `queue_users` (
+  `lesson_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `queue_order` int(11) NOT NULL,
+  KEY `lessonid_index` (`lesson_id`),
+  KEY `userid_index` (`user_id`),
+  CONSTRAINT `qu_lessonid_to_lessons_id` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`lesson_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `qu_userid_to_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `role_authorities`
+--
+
+DROP TABLE IF EXISTS `role_authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_authorities` (
+  `role_id` int(11) NOT NULL,
+  `authority_id` int(11) NOT NULL,
+  KEY `role_id_index` (`role_id`),
+  KEY `role_authorities_authority_id_to_authorities_authority_id` (`authority_id`),
+  CONSTRAINT `role_authorities_authority_id_to_authorities_authority_id` FOREIGN KEY (`authority_id`) REFERENCES `authorities` (`authority_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `role_authorities_role_id_to_roles_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subjects`
+--
+
+DROP TABLE IF EXISTS `subjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subjects` (
+  `subject_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `teacher` varchar(30) NOT NULL,
+  PRIMARY KEY (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  KEY `userid_index` (`user_id`),
+  KEY `user_roles_role_id_to_roles_role_id` (`role_id`),
+  CONSTRAINT `r_userid_to_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_roles_role_id_to_roles_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_index` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-04 10:33:44
