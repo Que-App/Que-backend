f09b376f96ff1c51788b173904af7c08c410b989
xsw2-2wsx <jan1undrych@gmail.com>
2021-01-10 23:49:11 +0100

    Allowed cross origin access for UserController

--- src/main/kotlin/app/api/v1/controllers/UserController.kt
+++ src/main/kotlin/app/api/v1/controllers/UserController.kt
@@ -4,10 +4,12 @@ import app.api.v1.pojos.UserPojo
 import app.api.v1.pojos.mapping.mapToPojo
 import app.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 class UserController {
 
