0e83a2f25dfff6fe3c9ed1dc916d4a0783c5437c
xsw2-2wsx <jan1undrych@gmail.com>
2021-01-16 01:24:33 +0100

    Changed password change request method to post

--- src/main/kotlin/app/api/v1/controllers/AccountController.kt
+++ src/main/kotlin/app/api/v1/controllers/AccountController.kt
@@ -7,7 +7,10 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import util.ok
 import javax.validation.Valid
 
@@ -22,7 +25,7 @@ class AccountController {
     @Autowired
     private lateinit var userService: UserService
 
-    @PatchMapping("/api/v1/account/changepass")
+    @PostMapping("/api/v1/account/changepass")
     fun changePassword(@Valid @RequestBody req: ChangePasswordRequest): ResponseEntity<Any?>
             = userService.changePassword(req.oldPassword!!, req.newPassword!!).ok()
 
