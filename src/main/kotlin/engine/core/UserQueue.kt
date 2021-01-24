868506d96ceff541403d8c4e38526366e34d0408
xsw2-2wsx <jan1undrych@gmail.com>
2021-01-24 10:18:02 +0100

    Added resetting pointer on IndexOutOfBoundsException (QUE-195)

--- src/main/kotlin/engine/core/UserQueue.kt
+++ src/main/kotlin/engine/core/UserQueue.kt
@@ -4,9 +4,15 @@ import app.data.entities.LessonEntity
 import engine.exceptions.EmptyQueueException
 import engine.util.Transaction
 import engine.util.UserTransaction
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import util.copy
 
 class UserQueue(val entity: LessonEntity, private val getIndex: () -> Int): Queue<Pair<Int, Int>> {
+
+    companion object {
+        private val log: Logger = LogManager.getLogger()
+    }
 
     override fun obtain(): Iterator<UserTransaction> = createIterator(entity)
 
@@ -20,9 +26,20 @@ class UserQueue(val entity: LessonEntity, private val getIndex: () -> Int): Queu
 
         override fun hasNext(): Boolean = true
 
-        override fun next(): UserTransaction = Transaction(entity.users[entity.pointer] to getIndex()) {
-            entity.pointer++
-            if(entity.pointer >= entity.users.size) entity.pointer = 0
+        //TODO: This can probably be done without code duplication. Don't want to use recursion though
+        // in case something goes terribly wrong
+        override fun next(): UserTransaction = try {
+            Transaction(entity.users[entity.pointer] to getIndex()) {
+                entity.pointer++
+                if (entity.pointer >= entity.users.size) entity.pointer = 0
+            }
+        } catch (e: IndexOutOfBoundsException) {
+            log.warn("IndexOutOfBounds exception encountered when retrieving user. Resetting pointer to 0.")
+            entity.pointer = 0;
+            Transaction(entity.users[entity.pointer] to getIndex()) {
+                entity.pointer++
+                if (entity.pointer >= entity.users.size) entity.pointer = 0
+            }
         }
     }
 
